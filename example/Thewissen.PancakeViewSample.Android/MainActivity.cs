7930223aa8a028338d099ef1458e355195ba48e8
sthewissen <github@thewissen.io>
2019-02-14 08:24:02 +0100

    Added app icons

--- example/Thewissen.PancakeViewSample.Android/MainActivity.cs
+++ example/Thewissen.PancakeViewSample.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Thewissen.PancakeViewSample.Droid
 {
-    [Activity(Label = "Thewissen.PancakeViewSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Thewissen.PancakeViewSample", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
