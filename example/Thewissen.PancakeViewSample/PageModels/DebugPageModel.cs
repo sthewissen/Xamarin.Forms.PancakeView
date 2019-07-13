37dee7fa3f9c454710d54906bad4c3a13ecea63e
sthewissen <github@thewissen.io>
2019-07-13 10:21:37 +0200

    Updated sample app

---
+++ example/Thewissen.PancakeViewSample/PageModels/DebugPageModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Thewissen.PancakeViewSample.PageModels
+{
+    public class DebugPageModel : FreshMvvm.FreshBasePageModel
+    {
+        private static readonly Random _randomGen = new Random();
+
+        public Color RandomColor
+        {
+            get
+            {
+                return GetRandomColor();
+            }
+        }
+
+        public ICommand CloseDebugModeCommand { get; set; }
+        
+        public DebugPageModel()
+        {
+            CloseDebugModeCommand = new Command(async (x) => await CoreMethods.PopPageModel(true, true));
+        }
+
+        public static Color GetRandomColor()
+        {
+            var color = Color.FromRgb((byte)_randomGen.Next(0, 255), (byte)_randomGen.Next(0, 255), (byte)_randomGen.Next(0, 255));
+            return color;
+        }
+    }
+}
---
+++ example/Thewissen.PancakeViewSample/Pages/DebugPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Thewissen.PancakeViewSample.Pages
+{
+    public partial class DebugPage : ContentPage
+    {
+        public DebugPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
