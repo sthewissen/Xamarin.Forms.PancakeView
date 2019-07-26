26c9b539b4d68871622a758353a7ab7690e12f72
sthewissen <github@thewissen.io>
2019-07-26 08:28:37 +0200

    Put Android parts in separate files

---
+++ src/Xamarin.Forms.PancakeView.Multi/Platforms/Android/RoundedCornerOutlineProvider.cs
@@ -0,0 +1,23 @@
+﻿using Android.Graphics;
+using Android.Views;
+
+namespace Xamarin.Forms.PancakeView.Droid
+{
+    public class RoundedCornerOutlineProvider : ViewOutlineProvider
+	{
+		private readonly float _cornerRadius;
+		private readonly int _border;
+
+		public RoundedCornerOutlineProvider(float cornerRadius, int border)
+		{
+			_cornerRadius = cornerRadius;
+			_border = border;
+		}
+
+		public override void GetOutline(global::Android.Views.View view, Outline outline)
+		{
+			// TODO: Figure out how to clip individual rounded corners with different radii.
+			outline.SetRoundRect(new Rect(0, 0, view.Width, view.Height), _cornerRadius);
+		}
+	}
+}
