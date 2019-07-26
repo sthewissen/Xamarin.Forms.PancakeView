e6a7905c4f534ac90527416a5ecd64f912b4f5dc
sthewissen <github@thewissen.io>
2019-07-26 08:47:30 +0200

    Removed strange project definition thing

--- src/Xamarin.Forms.PancakeView.Multi/Platforms/Android/RoundedCornerOutlineProvider.cs
+++ src/Xamarin.Forms.PancakeView.Multi/Platforms/Android/RoundedCornerOutlineProvider.cs
@@ -4,20 +4,20 @@ using Android.Views;
 namespace Xamarin.Forms.PancakeView.Droid
 {
     public class RoundedCornerOutlineProvider : ViewOutlineProvider
-	{
-		private readonly float _cornerRadius;
-		private readonly int _border;
+    {
+        private readonly float _cornerRadius;
+        private readonly int _border;
 
-		public RoundedCornerOutlineProvider(float cornerRadius, int border)
-		{
-			_cornerRadius = cornerRadius;
-			_border = border;
-		}
+        public RoundedCornerOutlineProvider(float cornerRadius, int border)
+        {
+            _cornerRadius = cornerRadius;
+            _border = border;
+        }
 
-		public override void GetOutline(global::Android.Views.View view, Outline outline)
-		{
-			// TODO: Figure out how to clip individual rounded corners with different radii.
-			outline.SetRoundRect(new Rect(0, 0, view.Width, view.Height), _cornerRadius);
-		}
-	}
+        public override void GetOutline(global::Android.Views.View view, Outline outline)
+        {
+            // TODO: Figure out how to clip individual rounded corners with different radii.
+            outline.SetRoundRect(new Rect(0, 0, view.Width, view.Height), _cornerRadius);
+        }
+    }
 }
