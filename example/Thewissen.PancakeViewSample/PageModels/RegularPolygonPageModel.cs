d09a4d26a5659746be9c6d2017c0b12628f527e0
Shanmukha Ranganath <shanmukharanganath@gmail.com>
2019-08-09 13:49:52 +0530

    Adds Polygon sample page

--- example/Thewissen.PancakeViewSample/PageModels/RegularPolygonPageModel.cs
+++ example/Thewissen.PancakeViewSample/PageModels/RegularPolygonPageModel.cs
@@ -1,8 +1,51 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Thewissen.PancakeViewSample.PageModels
 {
     public class RegularPolygonPageModel : FreshMvvm.FreshBasePageModel
     {
+        public ICommand ClosePolygonPageCommand { get; set; }
+
+        public RegularPolygonPageModel()
+        {
+            ClosePolygonPageCommand = new Command(async (x) => await CoreMethods.PopPageModel(true, true));
+        }
+
+
+        private int _sides = 6;
+        private double _offsetAngle;
+        private double _cornerRadius;
+
+        public int Sides
+        {
+            get => _sides;
+            set
+            {
+                _sides = value;
+                RaisePropertyChanged("Sides");
+            }
+        }
+
+        public double OffsetAngle
+        {
+            get => _offsetAngle;
+            set
+            {
+                _offsetAngle = value;
+                RaisePropertyChanged("OffsetAngle");
+            }
+        }
+
+        public double CornerRadius
+        {
+            get => _cornerRadius;
+            set
+            {
+                _cornerRadius = value;
+                RaisePropertyChanged("CornerRadius");
+            }
+        }
     }
 }
